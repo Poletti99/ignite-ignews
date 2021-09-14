e307845e63f3ab4ba67bc64447d13bbae2ee4cce
Victor Poletti <victorpoletti2009@hotmail.com>
2021-09-14 07:59:01 -0300

    configurando ambiente de testes

---
+++ jest.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  testIgnorePatterns: ['/node_modules/', '/.next/'],
+  setupFilesAfterEnv: ['<rootDir>/src/__tests__/setupTests.ts'],
+  transform: {
+    '^.+\\.(js|jsx|ts|tsx)$': '<rootDir>/node_modules/babel-jest',
+  },
+  testEnvironment: 'jsdom',
+};
