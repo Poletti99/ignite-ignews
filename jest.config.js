88ae00edce7a85dc924d26c126ad1b55a8599de1
Victor Poletti <victorpoletti2009@hotmail.com>
2021-09-14 19:44:50 -0300

    tests: testes para a pagina home

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  testPathIgnorePatterns: ['/node_modules/', '/.next/', '/__tests__/'],
+  testPathIgnorePatterns: ['/node_modules/', '/.next/', '/__tests__/setupTests.ts'],
   setupFilesAfterEnv: ['<rootDir>/src/__tests__/setupTests.ts'],
   transform: {
     '^.+\\.(js|jsx|ts|tsx)$': '<rootDir>/node_modules/babel-jest',
